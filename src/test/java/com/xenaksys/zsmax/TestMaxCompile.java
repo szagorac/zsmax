1074e5dfa70f1d386b6b01e143565c3bab9c2fc5
slavko <slavko@zagorac.com>
2020-04-27 19:19:25 +0100

    project setup

--- src/test/java/com/xenaksys/zsmax/TestMaxCompile.java
+++ src/test/java/com/xenaksys/zsmax/TestMaxCompile.java
@@ -1,6 +1,11 @@
 package com.xenaksys.zsmax;
 
 import com.cycling74.max.MaxObject;
+import com.cycling74.max.MaxSystem;
 
 public class TestMaxCompile extends MaxObject {
+
+    public void logError(String message) {
+        MaxSystem.error(message);
+    }
 }
